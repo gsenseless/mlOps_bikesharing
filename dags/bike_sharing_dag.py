ebca026cd8ab1c5b8709298eb8f149e235bac358
gsenseless <anton.petrov@smartmail.biz>
2024-09-03 10:52:06 +0300

    replace print with logging. Get bucket name from env.

--- dags/bike_sharing_dag.py
+++ dags/bike_sharing_dag.py
@@ -1,6 +1,7 @@
 import os
 import pickle
 from datetime import datetime
+import logging
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
@@ -8,11 +9,8 @@ from sklearn.metrics import mean_absolute_error
 from airflow.decorators import dag, task
 from airflow.io.path import ObjectStoragePath
 
-# from airflow.models.connection import Connection
-# import json
 
-
-bucket_name = "bike-sharing-model"
+bucket_name = os.getenv("BUCKET_NAME")
 
 
 @dag(
@@ -39,26 +37,28 @@ def bikes_taskflow_api():
         y = data["Rented Bike Count"]  # Target variable
 
         data_split = train_test_split(X, y, test_size=0.2, random_state=52)
-        train_x, test_x, train_y, test_y = data_split
 
+        logging.info(f"uploading data to s3 into {bucket_name} bucket")
         for name, data in zip(["train_x", "test_x", "train_y", "test_y"], data_split):
             data.to_csv(f"s3://{bucket_name}/{name}.csv")
 
+        logging.info("data is saved to s3")
         return None
 
     @task
     def train_model() -> str:
-        # load data
+        logging.info("loading data from s3")
         data = {}
         for name in ["train_x", "train_y"]:
             data[name] = pd.read_csv(f"s3://{bucket_name}/{name}.csv")
 
+        logging.info("training the model")
         model = DecisionTreeRegressor()
         model.fit(data["train_x"], data["train_y"])
 
-        # Save the model as a pickle file
-        base = ObjectStoragePath(f"s3://{bucket_name}/")
-        model_filename = "bike_sharing_model.pkl"
+        logging.info("saving the model to s3")
+        model_filename = "bike-sharing-model.pkl"
+        base = ObjectStoragePath(f"s3://{bucket_name}", conn_id=None)
         path = base / model_filename
         with path.open("wb") as f:
             pickle.dump(model, f)
@@ -67,12 +67,12 @@ def bikes_taskflow_api():
 
     @task
     def evaluate_model(model_filename: str) -> None:
-        # load test data
+        logging.info("loading test data")
         data = {}
         for name in ["test_x", "test_y"]:
             data[name] = pd.read_csv(f"s3://{bucket_name}/{name}.csv")
 
-        # Load the model from the pickle file
+        logging.info("loading the model")
         base = ObjectStoragePath(f"s3://{bucket_name}/")
         path = base / model_filename
         with path.open("rb") as f:
@@ -80,7 +80,7 @@ def bikes_taskflow_api():
 
         predictions = model.predict(data["test_x"])
         mae = mean_absolute_error(data["test_y"], predictions)
-        print(f"Mean Absolute Error: {mae}")
+        logging.info(f"Mean Absolute Error: {mae}")
         return None
 
     model_filename = train_model()
